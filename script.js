ca2b9153a7279f854b66126ad0ac14a20c20eab5
Subsuurya <136948468+Subsuurya@users.noreply.github.com>
2024-08-19 15:48:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add smooth scrolling to internal links
+document.addEventListener('DOMContentLoaded', () => {
+    const smoothScroll = (event) => {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 60, // Adjust offset if needed
+            behavior: 'smooth'
+        });
+    };
+
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => link.addEventListener('click', smoothScroll));
+});
