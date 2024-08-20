6e0f736ac815cca1135abc6f766b859f596fdc98
Subsuurya <136948468+Subsuurya@users.noreply.github.com>
2024-08-20 16:03:46 +0800

    Add files via upload
    2nd

--- script.js
+++ script.js
@@ -13,4 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => link.addEventListener('click', smoothScroll));
+    
+    
 });
